fd522de09421e65dee43606a27f45dd106ed49d3
benedict_mbmid2015 <shikifuchi@me.com>
2022-09-03 17:40:42 +0800

    track init.lua

---
+++ lua/benedict/lsp/init.lua
@@ -0,0 +1,22 @@
+local status_ok, _ = pcall(require, "lspconfig")
+if not status_ok then
+  vim.notify("lspconfig is missing")
+  return
+end
+
+
+-- require("benedict.lsp.lsp-installer")
+-- require("benedict.lsp.configs")
+--local handlers = require("benedict.lsp.handlers")
+--handlers.setup()
+require("mason").setup()
+require("mason-lspconfig").setup()
+-- require("benedict.lsp.null-ls")
+local status_ok, _ = pcall(require, "lspconfig")
+if not status_ok then
+  vim.notify("lspconfig is missing")
+  return
+end
+require("benedict.lsp.configs")
+local handlers = require("benedict.lsp.handlers")
+handlers.setup()
