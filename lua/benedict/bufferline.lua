2e22f2f0c0bf9ebfe14a733b76febb0ef828c7d2
benedict_mbmid2015 <shikifuchi@me.com>
2022-09-10 23:22:08 +0800

    buffer line added

---
+++ lua/benedict/bufferline.lua
@@ -0,0 +1,10 @@
+local ok, buffer_line = pcall(require, "bufferline")
+
+if not ok then
+  vim.notify("bufferline is missing")
+  return
+end
+
+
+vim.opt.termguicolors = true
+buffer_line.setup {}
--- lua/benedict/plugins.lua
+++ lua/benedict/plugins.lua
@@ -48,10 +48,7 @@ return packer.startup(function(use)
   use "wbthomason/packer.nvim" -- Have packer manage itself
   use "nvim-lua/plenary.nvim" -- usefule lua functions used by lots of plugins
   use 'nvim-lua/popup.nvim'
-  --  use({ "numToStr/Comment.nvim", commit = "2c26a00f32b190390b664e56e32fd5347613b9e2" })
-  --	use({ "JoosepAlviste/nvim-ts-context-commentstring", commit = "88343753dbe81c227a1c1fd2c8d764afb8d36269" })
-  --	use({ "kyazdani42/nvim-web-devicons", commit = "8d2c5337f0a2d0a17de8e751876eeb192b32310e" })
-  --	use({ "kyazdani42/nvim-tree.lua", commit = "bdb6d4a25410da35bbf7ce0dbdaa8d60432bc243" })
+  use { 'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons' }
   --	use({ "akinsho/bufferline.nvim", commit = "c78b3ecf9539a719828bca82fc7ddb9b3ba0c353" })
   --	use({ "moll/vim-bbye", commit = "25ef93ac5a87526111f43e5110675032dbcacf56" })
   --	use({ "nvim-lualine/lualine.nvim", commit = "3362b28f917acc37538b1047f187ff1b5645ecdd" })
@@ -69,7 +66,7 @@ return packer.startup(function(use)
   use "sharkdp/fd"
   -- Colorschemes
   use "folke/tokyonight.nvim"
-  -- use "lunarvim/darkplus.nvim"
+  use "lunarvim/darkplus.nvim"
 
   -- Markdown plugins
   use "ellisonleao/glow.nvim" -- markdown preview
@@ -105,13 +102,8 @@ return packer.startup(function(use)
   -- Only support linux so it is not very useful
   -- use "nvim-telescope/telescope-media-files.nvim"
   -- Treesitter
-  --	use({
-  --		"nvim-treesitter/nvim-treesitter",
-  --		commit = "518e27589c0463af15463c9d675c65e464efc2fe",
-  --	})
   use "nvim-treesitter/nvim-treesitter"
   -- Git
-  --	use({ "lewis6991/gitsigns.nvim", commit = "c18e016864c92ecf9775abea1baaa161c28082c3" })
   use("lewis6991/gitsigns.nvim")
   -- Comment plugins
   use("numToStr/Comment.nvim")
