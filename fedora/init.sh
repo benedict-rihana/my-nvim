2ea7a8dc38c2e5546724cf30775d89d6a0e7a9c1
Benedict <shikifuchi@hotmail.com>
2022-12-27 04:35:43 +0800

    adjust the script and add tree-sitter-cli

--- fedora/init.sh
+++ fedora/init.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlgeorge/ripgrep/repo/epel-7/carlgeorge-ripgrep-epel-7.repo
+echo "sherlocked" | sudo -S yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlgeorge/ripgrep/repo/epel-7/carlgeorge-ripgrep-epel-7.repo
 echo "sherlocked" | sudo -S dnf install ripgrep -y
 
 echo "\n"
 
 sudo npm install -g --save-dev --save-exact prettier
 sudo npm install -g clang-format
+sudo npm install -g tree-sitter-cli
 sudo npm install -g remark-cli remark-preset-lint-consistent remark-preset-lint-recommended remark-lint-list-item-indent
 sudo npm install -g glow
 
