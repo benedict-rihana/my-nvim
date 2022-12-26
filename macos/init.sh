0359abda1fe5b43ad693c1984cf9d44f6a14c1df
Benedict <shikifuchi@hotmail.com>
2022-12-27 04:46:30 +0800

    add tree-sitter

--- macos/init.sh
+++ macos/init.sh
@@ -4,6 +4,7 @@ brew install prettier
 brew install ripgrep
 
 npm install -g clang-format
+npm install -g tree-sitter-cli
 npm install -g remark-cli remark-preset-lint-consistent remark-preset-lint-recommended remark-lint-list-item-indent
 npm install -g glow
 pip3 install flake8
