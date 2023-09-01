a44b108cba7c3ef3c2a9139b1fd29987d51e4b37
Benedict <shikifuchi@hotmail.com>
2023-09-01 16:05:13 +0800

    update install script

--- fedora/init.sh
+++ fedora/init.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-#echo "sherlocked" | sudo -S yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlgeorge/ripgrep/repo/epel-7/carlgeorge-ripgrep-epel-7.repo
-echo "sherlocked" | sudo -S dnf install ripgrep -y
+read -s -p "please enter root password to install softwares" ROOTPASSWD;printf "%b" "\n"
+
+echo $ROOTPASSWD | sudo -S yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlgeorge/ripgrep/repo/epel-7/carlgeorge-ripgrep-epel-7.repo
+echo $ROOTPASSWD | sudo -S dnf install ripgrep -y
 
 echo "\n"
 
 sudo npm install -g --save-dev --save-exact prettier
 sudo npm install -g clang-format
 # sudo npm install -g tree-sitter-cli
-cargo install tree-sitter-cli
+echo $ROOTPASSWD | sudo dnf install tree-sitter-cli -y
 sudo npm install -g remark-cli remark-preset-lint-consistent remark-preset-lint-recommended remark-lint-list-item-indent
 sudo npm install -g glow
 
